217e2d3c53eafe71a5e33857290f12feeb585962
Dharmik <dharmikgodhani071@gmail.com>
2026-02-07 13:44:44 +0530

    fix issues

--- next.config.js
+++ next.config.js
@@ -6,26 +6,31 @@ dotenv.config()
 module.exports = {
   trailingSlash: true,
   reactStrictMode: false,
-  swcMinify: false,
-  output: 'export', // Forces Next.js to generate static files
+  swcMinify: true,
   experimental: {
     esmExternals: false,
     jsconfigPaths: true
   },
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx'], // Ensures Next.js finds pages inside src/pages
+
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+
   webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+      apexcharts: path.resolve(
+        __dirname,
+        './node_modules/apexcharts-clevision'
+      )
     }
 
     return config
   },
+
   env: {
     API_BASE_URL: process.env.API_BASE_URL
   },
+
   images: {
-    domains: ['localhost', '64.227.171.118'],
-    unoptimized: true // Required for Firebase Hosting
+    domains: ['localhost', '31.97.206.114', 'auction.workglyph.com']
   }
 }
